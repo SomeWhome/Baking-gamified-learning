0a591bf9298162218716851b8ef44c9868bf1f7d
SomeWhome <oliverhopkins2009@gmail.com>
2026-04-16 15:49:38 +0100

    Movement and crates working

---
+++ Baking gamified learning/Assets/Scripts/Crates.cs
@@ -0,0 +1,53 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Crates : MonoBehaviour
+{
+    public GameObject crate;
+    public GameObject spawnedObject;
+    private Vector2 SpawnPosition;
+    public Quaternion spawnedRotation = Quaternion.identity;
+    public KeyCode activationKey = KeyCode.E;
+    private bool entered =  false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crate != null)
+        {
+            SpawnPosition = crate.transform.position;
+
+            float x = SpawnPosition.x;
+            float y = SpawnPosition.y;
+            SpawnPosition.y =+ 0.1f;
+          
+        }
+        else
+        {
+            Debug.LogWarning("Target object is not assigned");
+        }
+
+        if (entered && Input.GetKeyDown(activationKey))
+        {
+            Instantiate(spawnedObject, SpawnPosition, crate.transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        entered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        entered = false;
+    }
+}
+
+
